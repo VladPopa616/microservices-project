889dc31db0314a37d2137d9f73579e7140a2ca28
VladPopa616 <vpopa18@gmail.com>
2021-08-25 17:05:35 -0400

    Added JUnit Test for ProductServiceImpl.

---
+++ microservices/product-service/src/test/java/com/popa/microservices/core/product/businesslayer/ProductServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.popa.microservices.core.product.businesslayer;
+
+import com.popa.api.core.product.Product;
+import com.popa.microservices.core.product.datalayer.ProductEntity;
+import com.popa.microservices.core.product.datalayer.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class ProductServiceImplTest {
+    @MockBean
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductService productService;
+
+    @Test
+    public void testGetProductByIdValid(){
+        //arrange
+        ProductEntity entity = new ProductEntity(1, "n", 1);
+        when(productRepository.findByProductId(1)).thenReturn(Optional.of(entity));
+
+        //act
+        Product returnedProduct = productService.getProductById(1);
+
+        //assert
+        assertThat(returnedProduct.getProductId()).isEqualTo(1);
+    }
+}
